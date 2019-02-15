0cd6ce4209806f2f259581f5574aaa852b2065b8
GurgenManukyan <gurgen.manukyang@gmail.com>
2019-02-15 13:57:15 +0400

    add index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<ul>
+		<li><a href="#">item1</a></li>
+		<li><a href="#">item2</a></li>
+		<li><a href="#">item3</a></li>
+	</ul>
+</body>
+</html>
