5fc6cb1948bb7bd9150337e7abc4353af546ce4f
GurgenManukyan <gurgen.manukyang@gmail.com>
2019-02-15 16:01:25 +0400

    start

--- index.php
+++ index.php
@@ -2,13 +2,61 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>CodeIt - Web Programming Center</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<ul>
-		<li><a href="#">item1</a></li>
-		<li><a href="#">item2</a></li>
-		<li><a href="#">item3</a></li>
-	</ul>
+	<header>
+		<nav class="navbar navbar-expand-lg ">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">CodeIT</a>
+
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#about">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">About</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link " href="#">Contact</a>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<div id="banner" class="text-center">
+			<div class="text">	
+				<h1>Code <span>IT</span> Web Programming </h1>	
+				<p>Make your code with us</p>
+			</div>	
+			<button>See More</button>
+		</div>
+	</header>	
+	<main>
+		<section class="container">
+			
+			<div class="row" id="about">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm12 col">
+					<div class="text-center">
+						<h2>About Us</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus maiores incidunt natus excepturi ab soluta consequatur veniam, quidem eveniet, maxime magnam consectetur velit ipsa, pariatur magni alias cupiditate officiis unde!</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>	
+
+
+
+
+
+<script  src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
